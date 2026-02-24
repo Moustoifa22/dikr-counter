d2cb7f7cf7c7c21a869ff99db243320126085f0d
Moustoifa22 <moustoifa.ali-djoumoi.auditeur@lecnam.net>
2026-02-24 22:47:11 +0100

    Update service worker to include new icon files
    Added new icons to the service worker cache and implemented cache cleanup on activation.

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,22 @@
 const CACHE = 'dikr-v1';
-const FILES = ['./dikr_counter.html', './manifest.json'];
+const FILES = [
+  './dikr_counter.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+});
+
+// Nettoyage des anciens caches lors de la mise à jour
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
 });
 
 self.addEventListener('fetch', e => {
